3d4863f40e6292ac3ef6a3b0a2549a2c85f4c22b
damienmillet <contact@damien-millet.dev>
2023-03-15 17:23:35 +0000

    🍱  update http

--- http/mod.ts
+++ http/mod.ts
@@ -1,86 +1,22 @@
-abstract class Http {
-  declare url: string;
-  declare headers: Headers;
-  declare token: string;
-  declare response: Response;
+export type http = {
+  url?: string;
+  headers: Headers;
+  response?: Response;
+  fetchJson<T>(url: string, options: RequestInit): Promise<T>;
+  fetch(url: string, options: RequestInit): Promise<Response>;
+};
 
-  constructor(url: string, headers: HeadersInit, token?: string) {
-    this.url = url;
-    this.headers = new Headers(headers);
-    token && (this.token = token);
-  }
+const Http: http = {
+  headers: new Headers({ "Content-Type": "application/json" }),
+  async fetchJson<T>(url: string, options: RequestInit) {
+    await this.fetch(url, options);
+    return await this.response?.json() as Promise<T>;
+  },
+  async fetch(url: string, options: RequestInit) {
+    options.headers = Object.assign(this.headers, options.headers);
+    this.response = await fetch(url, options);
+    return this.response;
+  },
+};
 
-  get ok() {
-    return this.response.ok;
-  }
-
-  get status() {
-    return this.response.status;
-  }
-
-  get statusText() {
-    return this.response.statusText;
-  }
-
-  async get(uri: string, headers = new Headers()) {
-    return await this.fetch(uri, { method: 'GET', headers: headers });
-  }
-
-  async post(
-    uri: string,
-    body: Record<never, never>,
-    headers = new Headers(),
-  ) {
-    return await this.fetch(uri, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: headers,
-    });
-  }
-
-  async put(
-    uri: string,
-    body: Record<never, never>,
-    headers = new Headers(),
-  ) {
-    return await this.fetch(uri, {
-      method: 'PUT',
-      body: JSON.stringify(body),
-      headers: headers,
-    });
-  }
-
-  async delete(uri: string, headers = new Headers()) {
-    return await this.fetch(uri, { method: 'DELETE', headers: headers });
-  }
-
-  async options<T = Record<never, never>>(
-    uri: string,
-    headers = new Headers(),
-  ) {
-    return await this.fetch(uri, { method: 'OPTIONS', headers: headers });
-  }
-
-  async patch(
-    uri: string,
-    body: Record<never, never>,
-    headers = new Headers(),
-  ) {
-    return await this.fetch(uri, {
-      method: 'PATCH',
-      body: JSON.stringify(body),
-      headers: headers,
-    });
-  }
-
-  async fetch(uri: string, options: RequestInit) {
-    return await fetch(uri, options);
-  }
-
-  abstract fetchJson<T>(
-    uri: string,
-    options: RequestInit,
-  ): Promise<T>;
-}
-
-export { Http };
+export default Http;
