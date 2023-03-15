8ad8bee00f43d960b644655a9adc5d8b7e1d03b7
damienmillet <contact@damien-millet.dev>
2023-03-15 18:34:48 +0000

    🌱  update export mod file

--- mod.ts
+++ mod.ts
@@ -1,58 +1,10 @@
-export { debug } from "./debug/mod.ts";
-export { Match, Mathx } from "./math/mod.ts";
-export { Mox } from "./mox/mod.ts";
-export { existFile, existFolder } from "./file/mod.ts";
-export { type http, Http } from "./http/mod.ts";
-export {
-  type ws,
-  type message,
-  type sendMessage,
-  type sendMsg,
-  type mes,
-  type subscribe,
-  Ws,
-} from "./ws/mod.ts";
-export { Mongo } from "./mongo/mod.ts";
-export {
-  upper,
-  same,
-  equal,
-  lower,
-  upperOrSame,
-  lowerOrSame,
-  upperOrEqual,
-  lowerOrEqual,
-} from "./compare/mod.ts";
-export {
-  removeLongerThan,
-  getByIndex,
-  getLast,
-  getFirst,
-  findIndexById,
-  findIndexByName,
-  findByIndex,
-  findById,
-  remove,
-  removeOne,
-  add,
-  addAll,
-  find,
-  findIndex,
-  findFirst,
-  findLast,
-  sortById,
-  sortByReverseId,
-  getByReverseIndex
-
-} from "./array/mod.ts";
-export {
-  minNeededByDay,
-  minNeededByHour,
-  minNeededByMinute,
-  minNeededByMonth,
-  average,
-  sum,
-  min,
-  max,
-  median,
-} from "./statistic/mod.ts";
+export * from "./debug/mod.ts";
+export * from "./math/mod.ts";
+export * from "./mox/mod.ts";
+export * from "./file/mod.ts";
+export * from "./http/mod.ts";
+export * from "./ws/mod.ts";
+export * from "./mongo/mod.ts";
+export * from "./compare/mod.ts";
+export * from "./array/mod.ts";
+export * from "./statistic/mod.ts";
