73b70dabdd259b0b6415a66025e851deb6a3838d
damienmillet <contact@damien-millet.dev>
2023-03-15 17:56:40 +0000

    🍱  update file asset

--- file/mod.ts
+++ file/mod.ts
@@ -1,9 +1,16 @@
-export const File = {
-  existFolder: (folder: string) => {
-    try {
-      Deno.readDirSync(folder);
-    } catch (_error) {
-      Deno.mkdirSync(folder);
-    }
+
+export const existFolder = (folder: string) => {
+  try {
+    Deno.readDirSync(folder);
+  } catch (_error) {
+    Deno.mkdirSync(folder);
+  }
+}
+
+export const existFile = (file: string) => {
+  try {
+    return Deno.readFileSync(file);
+  } catch (_error) {
+    Deno.writeFileSync(file, new Uint8Array());
   }
 }
--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 export { debug } from "./debug/mod.ts";
 export { Match, Mathx } from "./math/mod.ts";
 export { Mox } from "./mox/mod.ts";
-export { File } from "./file/mod.ts";
+export { existFile, existFolder } from "./file/mod.ts";
 export { type http, Http } from "./http/mod.ts";
 export {
   type ws,
