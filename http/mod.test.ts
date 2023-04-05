f28eb42d9e002e57fef660f40b8895247f8abd95
damienmillet <contact@damien-millet.dev>
2023-04-05 15:04:35 +0000

    ✨ now autoadd cookie when Set-Cookie is used

--- http/mod.test.ts
+++ http/mod.test.ts
@@ -2,7 +2,7 @@ import * as test from "https://deno.land/std@0.181.0/testing/asserts.ts";
 import * as mod from "./mod.ts";
 
 Deno.test("fetch", async () => {
-  const res = await mod.Http.fetch("https://jsonplaceholder.typicode.com/todos/1", {});
+  const res = await mod.Http.fetch("https://jsonplaceholder.typicode.com/todos/1");
   const json = await res.json();
   test.assert(res.ok);
   test.assert(json.id === 1);
@@ -11,7 +11,13 @@ Deno.test("fetch", async () => {
 
 
 Deno.test("fetchJson", async () => {
-  const json = await mod.Http.fetchJson<{ id: number }>("https://jsonplaceholder.typicode.com/todos/1", {});
+  const json = await mod.Http.fetchJson<{ id: number }>("https://jsonplaceholder.typicode.com/todos/1");
   test.assert(json.id === 1);
+})
+
+Deno.test("fetchJson test setCookie", async () => {
+  const http = mod.Http;
+  const json = await http.fetchJson<{ status: string }>("https://oc.damien-millet.dev/api/setcookie");
+  test.assert(http.headers.get("cookie"))
 })
 
--- http/mod.ts
+++ http/mod.ts
@@ -2,19 +2,27 @@ export type http = {
   url?: string;
   headers: Headers;
   response?: Response;
-  fetchJson<T>(url: string, options: RequestInit): Promise<T>;
-  fetch(url: string, options: RequestInit): Promise<Response>;
+  fetchJson<T>(url: string, options?: RequestInit): Promise<T>;
+  fetch(url: string, options?: RequestInit): Promise<Response>;
+  setCookies(cookies: string): void;
 };
 
 export const Http: http = {
   headers: new Headers({ "Content-Type": "application/json" }),
-  async fetchJson<T>(url: string, options: RequestInit) {
+  async fetchJson<T>(url: string, options?: RequestInit) { // RequestInit
     await this.fetch(url, options);
     return await this.response?.json() as Promise<T>;
   },
-  async fetch(url: string, options: RequestInit) {
-    options.headers = Object.assign(this.headers, options.headers);
+  async fetch(url: string, options?: RequestInit) {
+    if (!options) options = {};
+    options.headers = Object.assign(this.headers, options?.headers);
     this.response = await fetch(url, options);
+    if (this.response.headers.get("set-cookie")) {
+      this.setCookies(this.response.headers.get("set-cookie")!);
+    }
     return this.response;
   },
+  setCookies(cookies: string) {
+    this.headers.set("cookie", cookies);
+  }
 };
